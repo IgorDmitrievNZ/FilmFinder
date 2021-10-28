a6ed410489074998fa9ea6acde582baf019efe54
Igor <idmitrievnz@gmail.com>
2021-10-28 20:35:04 +1300

    Added retrofit and coil functionality

--- app/src/main/java/com/example/android/filmfinder/model/entities/Film.kt
+++ app/src/main/java/com/example/android/filmfinder/model/entities/Film.kt
@@ -5,17 +5,58 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Film(
-    val id: String,
-    val poster: String
+    val id: String
 ) : Parcelable
 
-fun getDefaultMovie() = Film(
-    "tt1375666",
-    "https://images.freeimages.com/images/large-previews/241/night-fog-1521028.jpg"
-)
+fun getDefaultMovie() = Film("tt1375666")
 
 fun getTopMovies() = listOf(
-    MovieFinder(Film("tt1375666", "poster"), "Inception", 2010, 8.8, "description"),
-    MovieFinder(Film("tt0109830", "poster"), "Forrest Gump", 1994, 8.8, "description")
+    MovieFinder(
+        Film("tt1375666"),
+        "Inception",
+        2010,
+        8.8,
+        "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@.jpg"
+    ),
 
-)
+    MovieFinder(
+        Film("tt0109830"),
+        "Forrest Gump",
+        1994,
+        8.8,
+        "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@.jpg"
+    ),
+
+    MovieFinder(
+        Film("tt0111161"),
+        "The Shawshank Redemption",
+        1994,
+        9.3,
+        "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg"
+    ),
+
+    MovieFinder(
+        Film("tt0137523"),
+        "Fight Club",
+        1999,
+        8.8,
+        "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@.jpg"
+    ),
+
+    MovieFinder(
+        Film("tt0120689"),
+        "The Green Mile",
+        1999,
+        8.6,
+        "https://m.media-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1_UX182_CR0,0,182,268_AL_.jpg"
+    ),
+
+    MovieFinder(
+        Film("tt0133093"),
+        "The Matrix",
+        1999,
+        8.7,
+        "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@.jpg"
+    ),
+
+    )
--- app/src/main/java/com/example/android/filmfinder/model/repository/RepositoryImp.kt
+++ app/src/main/java/com/example/android/filmfinder/model/repository/RepositoryImp.kt
@@ -1,17 +1,18 @@
 package com.example.android.filmfinder.model.repository
 
-import com.example.android.filmfinder.model.FilmLoader
 import com.example.android.filmfinder.model.entities.MovieFinder
 import com.example.android.filmfinder.model.entities.getTopMovies
+import com.example.android.filmfinder.model.rest.FilmRepo
 
 class RepositoryImp : Repository {
 
     override fun getMoviesFromServer(id: String): MovieFinder {
-        val dto = FilmLoader.loadFilm(id)
+        val dto = FilmRepo.api.getMovie(id).execute().body()
         return MovieFinder(
             title = dto?.title,
             year = dto?.year ?: 0,
             rating = dto?.rating ?: 0.0,
+            poster = dto?.poster,
             plot = dto?.plot
         )
     }
